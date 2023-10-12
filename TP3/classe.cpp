fb8571ae1a0a774653950abcd07736abe1b7c664
RemiMarty <marty.remi@lyceejulesfil.Fr>
2023-10-12 10:48:15 +0200

    TP 3 language C++

---
+++ TP3/classe.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Personne
+{
+string m_nom;
+string m_prenom; unsigned short m_age;
+string m_profession; string m_adresse;
+};
---
+++ TP3/methode.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Personne
+{
+// Méthodes
+void demenager (string nouvelle_adresse)
+{
+}
+void anniversaire()
+{
+}
+void changer_travail (string nouveau_travail)
+{
+}
+void marier(string nouveau_nom)
+{
+}
+// Attributs
+string m_nom;
+string m_prenom;
+unsigned short m_age;
+string m_profession; 
+string m_adresse;
+};
